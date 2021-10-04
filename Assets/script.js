a0ec48dd28726b41a2346ac748c989c8c52cacf6
Nick Margaritondo <86981780+Nickm615@users.noreply.github.com>
2021-10-03 19:34:57 -0500

    Cleaned up code. Separated things into separate functions for readability and used parameters/arguments to carry data between functions

--- Assets/script.js
+++ Assets/script.js
@@ -1,41 +1,60 @@
 var mainEl = document.querySelector('main')
 var weatherToday = document.getElementById('weatherToday')
-var lat;
-var lon;
+var cityName = document.getElementById('cityEntry').value;
+var queryUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&units=imperial&appid=22a142603e1f1adab5d950df358023bb'
+var cityEntryBtn = document.getElementById('cityEntryBtn')
 
+cityEntryBtn.addEventListener('click', getCity);
 
-
-cityEntryBtn.addEventListener('click', function (event) {
-    var cityName = document.getElementById('cityEntry').value;
-    var queryUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&units=imperial&appid=22a142603e1f1adab5d950df358023bb'
+function getCity(cityName) {
     fetch(queryUrl)
         .then(function (response) {
+            console.log(response.json)
             return (response.json())
         })
         .then(function (data) {
-            console.log(cityName)
+            console.log(cityEntry.value);
             console.log(data);
-            var cityName = data.name;
-            var h1El = document.createElement('h1')
-            h1El.textContent = cityName + ' ' + moment().format('MM-DD-YYYY')
-            weatherToday.appendChild(h1El);
-            var temp = data.main.temp;
-            console.log(temp)
-            var pEl = document.createElement('p');
-            pEl.textContent = 'Temp: ' + Math.floor(temp);
-            weatherToday.appendChild(pEl);
-            var wind = data.wind.speed;
-            var pEl2 = document.createElement('p')
-            pEl2.textContent = 'Wind: ' + wind;
-            weatherToday.appendChild(pEl2);
-            var humidity = data.main.humidity;
-            var pEl3 = document.createElement(pEl3)
-            pEl3.textContent = 'Humidity: ' + humidity;
-            weatherToday.appendChild(pEl3);
+            renderCityName(cityEntry.value)
+            renderTemp(data.main.temp)
+            renderWind(data.wind.speed)
+            renderHumidity(data.main.humidity)
             
-
-
-
         })
-})
 
+    }
+
+
+
+function renderCityName(cityName){
+    var h1El = document.createElement('h1');
+    h1El.textContent = cityName + ' ' + moment().format('MM-DD-YYYY');
+    weatherToday.appendChild(h1El);
+}
+
+function renderTemp(temp){
+    var pEl = document.createElement('p');
+    pEl.textContent = 'Temp: ' + Math.floor(temp);
+    weatherToday.appendChild(pEl);
+}
+function renderWind(wind){
+    var pEl2 = document.createElement('p');
+    pEl2.textContent = 'Wind: ' + wind;
+    weatherToday.appendChild(pEl2);
+}
+function renderHumidity(humidity){
+var pEl3 = document.createElement(pEl3);
+pEl3.textContent = 'Humidity: ' + humidity;
+weatherToday.appendChild(pEl3);
+
+}
+
+        
+//             // var cityName = data.name;
+//             
+//             
+//             var wind = data.wind.speed;
+//             
+//             var humidity = data.main.humidity;
+//          })
+// }
